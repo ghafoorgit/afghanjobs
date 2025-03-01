888ad4b9f0fff06e07437c4fe4032d0254ab2dc4
Talash <ghtalash@gmail.com>
2025-03-01 11:32:35 +0430

    The navbar is fixed

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="min-height: 80px;">
+    <div class="container">
+        <a class="navbar-brand text-white" href="{{ url('/') }}">Afghan Jobs</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto" style="width: 50%;"> <!-- Centering the search bar -->
+                <li class="nav-item w-100">
+                    <form class="d-flex w-100" action="{{ url('/search') }}" method="GET">
+                        <input class="form-control me-2 flex-grow-1" type="search" placeholder="Search job title" aria-label="Search" name="query">
+                        <button class="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ url('/jobs') }}">Jobs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ url('/about') }}">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ url('/contact-us') }}">Contact Us</a>
+                </li>
+
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ url('/login') }}">Login</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ url('/logout') }}"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,45 +18,7 @@
     </style>
 </head>
 <body>
-
-    <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand text-white" href="{{ url('/') }}">Afghan Jobs</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ url('/jobs') }}">Jobs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ url('/about') }}">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ url('/contact-us') }}">Contact Us</a>
-                    </li>
-
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ url('/login') }}">Login</a>
-                        </li>
-                    @else
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ url('/logout') }}"
-                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Page Content -->
+    <x-header />
     <div class="container">
         {{ $slot }}
     </div>
